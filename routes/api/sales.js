7349086720bb88fa344d5d9ca5bf174a383ec8fe
Maxim Surovtsev <realitybelieves@gmail.com>
2018-02-28 17:24:06 +0300

    /sales post request.

--- routes/api/sales.js
+++ routes/api/sales.js
@@ -20,4 +20,29 @@ router.get('/sales', (req, res) => {
     });
 });
 
+router.post('/sales', (req, res) => {
+  var sample = {
+    name: req.body.name,
+    category: req.body.category,
+    oldPrice: req.body.oldPrice,
+    newPrice: req.body.newPrice,
+    dateIn: req.body.dateIn,
+    dateOut: req.body.dateOut,
+    condition: req.body.condition,
+    image: req.body.image,
+    imageUrl: req.body.imageUrl,
+    discount: req.body.discount
+  };
+
+  models.Item.findOrCreate({
+    where: sample,
+    defaults: sample 
+  })
+    .spread((item, created) => {
+      // true if a new object was created and false if not
+      console.log(created);
+      res.json(item);
+    });
+});
+
 module.exports = router;
