8d10dcf3b5e0abc0be2ded179e524190429b43fd
Maxim Surovtsev <realitybelieves@gmail.com>
2018-02-28 16:50:43 +0300

    Body parser middleware.

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const models = require(path.join(__dirname, 'models'));
 const sales = require(path.join(__dirname, 'routes', 'api', 'sales'));
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
 
+app.use(bodyParser.json());
 app.use('/api', sales);
 
 app.listen(port, function() {
