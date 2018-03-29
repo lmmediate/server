ee1564042a0816193556ca9adc61d85dd2b4e18d
Maxim Surovtsev <realitybelieves@gmail.com>
2018-03-29 21:06:43 +0300

    Add/remove items from multiple shoplists.

--- routes/api/shoplist.js
+++ routes/api/shoplist.js
@@ -108,58 +108,83 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/add', (req, res) => {
-  var userId = req.user.id;
-
+router.post('/:id/add', (req, res) => {
+  var userId = req.user['id'];
+  var shoplistId = req.params['id'];
   var itemId = req.query['id'];
   var customItemName = req.query['custom'];
 
-  if(!customItemName && itemId) {
-    models.Account.findOne({ where: { id: userId } })
-      .then(user => {
+  models.Account.findOne({
+    where: { 
+      id: userId 
+    },
+    include: [{
+      model: models.ShopList,
+      where: {
+        id: shoplistId
+      }
+    }]
+  })
+    .then(user => {
+      var shoplist = user.shoplists.shift();
+      if(!customItemName && itemId) {
         models.Item.findOne({ where: { id: itemId } })
           .then(item => {
             if(item) {
-              user.addItem(item).then(() => res.sendStatus(200));
+              shoplist.addItem(item).then(() => res.sendStatus(200));
             } else {
               res.status(404).send('No such item');
             }
           });
-      });
-  } else if(!itemId && customItemName) {
-    models.Account.findOne({ where: { id: userId } })
-      .then(user => {
-        user.createCustomItem({item: customItemName})
-          .then(item => {
-            applyMatchingItems(item)
-              .then(a => res.json(a));
-          });
-      });
-  } else {
-    res.status(500).send('Missing query params.');
-  }
+      } else if(!itemId && customItemName) {
+        shoplist.createCustomItem({name: customItemName})
+          .then(item => applyMatchingItems(item))
+          .then(a => res.json(a));
+      } else {
+        res.status(500).send('Missing query params.');
+      }
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 
-router.delete('/delete', (req, res) => {
+router.delete('/:id/delete', (req, res) => {
   var userId = req.user.id;
   var itemId = req.query['id'];
   var customItemId = req.query['customid'];
+  var shoplistId = req.params['id'];
 
-  if(!customItemId && itemId) {
-    models.Account.findOne({ where: { id: userId } })
-      .then(user => {
-        user.removeItem(itemId)
+  console.log('\t' + itemId);
+
+  models.Account.findOne({
+    where: { 
+      id: userId 
+    },
+    include: [{
+      model: models.ShopList,
+      where: {
+        id: shoplistId
+      }
+    }]
+  })
+    .then(user => {
+      var shoplist = user.shoplists.shift();
+      if(!customItemId && itemId) {
+        shoplist.removeItems(itemId)
           .then(() => res.sendStatus(200));
-      });
-  } else if(!itemId && customItemId) {
-    models.Account.findOne({ where: { id: userId } })
-      .then(user => {
-        user.removeCustomItems(customItemId)
-          .then(() => res.sendStatus(200));
-      });
-  } else {
-    res.status(500).send('Missing query params.');
-  }
+      } else if(!itemId && customItemId) {
+            shoplist.removeCustomItems(customItemId)
+              .then(() => res.sendStatus(200));
+      } else {
+        res.status(500).send('Missing query params.');
+      }
+    })
+    .catch(error => res.status(500).send(error));
+  
 });
+
+// TODO: detele shoplist 
+// TODO: create shoplist
 
 module.exports = router;
